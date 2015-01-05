7470c5ebcb3fe899b2c76353e9d93f9c57d91b04
Mo <mo@Mos-MBPro.local>
2015-01-04 23:08:14 -0500

    Initial Commit

---
+++ BadToDoList/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BadToDoList
+//
+//  Created by mo on 1/4/15.
+//  Copyright (c) 2015 Mo Design. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
